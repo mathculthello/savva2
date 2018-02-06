7b0a5bd3f968b181e4965aa85e0cd78c12a5c4f2
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-02-06 12:28:10 +0300

    view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layout')
 
-<?php
-$a=['youtu']
- ?>
-      @section('content')
-      <h1>Ресурсы Алексея Савватеева</h1>
-
-      @include('index/sublist')
-      @endsection
+@section('content')
+<h1>Ресурсы Алексея Савватеева</h1>
+@include('index/sublist')
+@endsection
