9342412a93a4a4fcf5f4966f31a71652d7e33ce5
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-02-07 19:21:01 +0300

    google fix

--- app/Google.php
+++ app/Google.php
@@ -132,7 +132,10 @@ public function load_response()
     $data = self::get_response($this->url,$this->queryset);
     $result = json_decode($data);
 
+    if($result->items){
     $this->items = array_merge($this->items,$result->items);
+    }
+
     if(!$this->total){
     $this->total = $result->queries->nextPage[0]->totalResults;
     }
