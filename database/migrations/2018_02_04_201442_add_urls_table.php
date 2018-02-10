73d4cada5cf81bb44cce58fab9eb4eb71573f8e3
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-02-11 02:51:55 +0300

    incrementsfield

--- database/migrations/2018_02_04_201442_add_urls_table.php
+++ database/migrations/2018_02_04_201442_add_urls_table.php
@@ -14,6 +14,7 @@ class AddUrlsTable extends Migration
     public function up()
     {
         Schema::create('urls', function (Blueprint $table) {
+          $table->increments('id');
           $table->string('url')->unique();
           $table->string('title')->nullable();
           $table->integer('section')->nullable();
