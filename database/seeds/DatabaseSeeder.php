432ea85e7d71e88e35b43b6453f7280ce0409368
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-02-12 23:47:54 +0300

    added user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UrlsTableSeeder::class);
+      $this->call(UsersTableSeeder::class);
+      $this->call(UrlsTableSeeder::class);
     }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new Savva\User();
+        $user->name = 'egor';
+        $user->email = 'egor@egor.cf';
+        $user->password = Hash::make('egor');
+        $user->save();
+    }
+}
