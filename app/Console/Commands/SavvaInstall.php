7c84707720afac656b6925ec56b9e25ba585ad06
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-02-14 14:52:28 +0300

    savva install command

---
+++ app/Console/Commands/SavvaInstall.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Savva\Console\Commands;
+
+use Illuminate\Console\Command;
+use Encore\Admin\Console\InstallCommand as LaravelAdminInstallCommand;
+
+class SavvaInstall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'savva:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $laravel_admin_install = new LaravelAdminInstallCommand;
+        $laravel_admin_install->initDatabase();
+    }
+}
