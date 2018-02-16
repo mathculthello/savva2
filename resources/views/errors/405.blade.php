a843f43f539b5a06b2f04132b7172a95ffe23980
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-02-16 20:16:45 +0300

    405 img

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -5,8 +5,10 @@
 <h1>405</h1>
 
 <h2>
-  Такой страницы нет!
+  Метод недоступен!
 </h2>
+
+<img src="https://static1.squarespace.com/static/522d9f9fe4b00c944328b1d2/t/5241acbae4b0904c7813a011/1380035774446/out+with+the+old+and+in+with+the+new+multiplication.png?format=500w">
 
 <p>
   Попробуйте начать с <a href="/">главной страницы</a>!
