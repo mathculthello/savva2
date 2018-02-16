59834847a1ceeb2b1407286cdfc7d7008d5db646
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-02-16 20:04:46 +0300

    405 ошибка шаблон

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.simple')
+
+@section('content')
+
+<h1>405</h1>
+
+<h2>
+  Такой страницы нет!
+</h2>
+
+<p>
+  Попробуйте начать с <a href="/">главной страницы</a>!
+</p>
+
+@endsection
