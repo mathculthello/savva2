f9052eae52216218e3fe2c9fcd6e0bac2946e49a
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-02-16 23:05:08 +0300

    405

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -5,7 +5,7 @@
 <h1>405</h1>
 
 <h2>
-  Такого метода не существует!
+  Метод не найден!
 </h2>
 
 <img src="https://static1.squarespace.com/static/522d9f9fe4b00c944328b1d2/t/5241acbae4b0904c7813a011/1380035774446/out+with+the+old+and+in+with+the+new+multiplication.png?format=500w">
